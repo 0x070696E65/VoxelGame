3a97bbe1e8dd2327d99d4d197d000bfa543e225a
toshiya matsukawa <matsukawa5955@gmail.com>
2023-02-17 17:42:32 +0200

    paint

--- Assets/Scripts/PixelPaint/PixelPaint.cs
+++ Assets/Scripts/PixelPaint/PixelPaint.cs
@@ -24,27 +24,27 @@ public class PixelPaint : MonoBehaviour
     private Vector3 boaradBasePos;
     private Color brushColor;
     
-    public Texture2D texture;
-
     Vector2 _cursorPosition;
     public byte BoardSize = 16;
-    
-    [SerializeField] string filePath = "Assets/export.png";
+    private const string filePath = "Assets/texture";
     [SerializeField] private Button drawMode;
     [SerializeField] private Button eraceMode;
     [SerializeField] private Button allClear;
     [SerializeField] private Button save;
-    [SerializeField] private Button setAllTextures;
     private bool isDraw;
     
-    [SerializeField] private Button front;
-    [SerializeField] private Button back;
-    [SerializeField] private Button top;
-    [SerializeField] private Button botton;
-    [SerializeField] private Button left;
-    [SerializeField] private Button right;
+    [SerializeField] private List<Button> changeButton;
 
+    [SerializeField] private Image mainImage;
     [SerializeField] private List<Image> images;
+
+    [SerializeField] private List<Button> Mirrors;
+    [SerializeField] private List<Image> MirrorsImage;
+    private readonly List<byte> MirrorsState = new List<byte>{0,0,0,0,0,0};
+
+    private readonly List<Texture2D> textureList = new List<Texture2D>();
+    private readonly List<Sprite> spriteList = new List<Sprite>();
+    private byte textureNumber = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -71,16 +71,30 @@ public class PixelPaint : MonoBehaviour
             drawMode.interactable = true;
             eraceMode.interactable = false;
         });
-        allClear.onClick.AddListener(AllErace);
+        allClear.onClick.AddListener(()=>AllErace(textureList[textureNumber]));
         save.onClick.AddListener(SaveTecture);
         isDraw = true;
-        
-        setAllTextures.onClick.AddListener(SetAllTextures);
-    }
 
+        for (byte i = 0; i < changeButton.Count; i++) {
+            var i1 = i;
+            changeButton[i].onClick.AddListener(()=>SwitchTexture(i1));
+        }
+        for (byte i = 0; i < Mirrors.Count; i++) {
+            var i1 = i;
+            Mirrors[i].onClick.AddListener(()=>Mirror(i1));
+        }
+        
+    }
+    
     void InitCanvas()
     {
+        Clear();
         imageSize = offset / BoardSize;
+        initAllImages(16);
+        mainImage.sprite = spriteList[0];
+        for (var i = 0; i < 6; i++)
+            images[i].sprite = spriteList[0];
+        textureNumber = 0;
     }
 
     private void Update()
@@ -119,28 +133,18 @@ public class PixelPaint : MonoBehaviour
     {
         var mp = GetMousePositionInLocalSpace(Boarad);
         var v2i = new Vector2Int((int)(mp.x / imageSize), (int)(mp.y / imageSize));
-        var pixelData = texture.GetPixelData<Color32>( 0 );
+        var pixelData = textureList[textureNumber].GetPixelData<Color32>( 0 );
         pixelData[v2i.x + v2i.y * BoardSize] = isDraw ? brushColor : new Color(0, 0, 0, 0);
-        texture.Apply();
+        textureList[textureNumber].Apply();
     }
-
-    private void OnDestroy()
+    
+    private static void AllErace(Texture2D _texture)
     {
-        AllErace();
-        Resources.UnloadUnusedAssets();
-        foreach (var image in images)
-        {
-            Destroy(image.sprite);
-        }
-    }
-
-    private void AllErace()
-    {
-        var pixelData = texture.GetPixelData<Color32>( 0 );
+        var pixelData = _texture.GetPixelData<Color32>( 0 );
         for ( var i = 0; i < pixelData.Length; i++ ) {
             pixelData[ i ] = new Color32( 0, 0, 0, 0 );
         }
-        texture.Apply();
+        _texture.Apply();
     }
 
     private bool InBoard(Vector2 pos)
@@ -150,16 +154,27 @@ public class PixelPaint : MonoBehaviour
                && pos.y > boaradBasePos.y
                && pos.y < boaradBasePos.y + offset;
     }
+
+    private bool IsTransparent(Texture2D _texture)
+    {
+        var pixelData = _texture.GetPixelData<Color32>( 0 );
+        for ( var i = 0; i < pixelData.Length; i++ ) {
+            if (pixelData[i].b != 0 || pixelData[i].g != 0 || pixelData[i].r != 0 || pixelData[i].a != 0) return false;
+        }
+        return true;
+    }
     
     private void SaveTecture()
     {
-        ConvertTextureToPng(texture);
+        for (var i = 0; i < textureList.Count; i++) {
+            if (!IsTransparent(textureList[i])) ConvertTextureToPng(textureList[i], i.ToString());
+        }
     }
     
-    void ConvertTextureToPng(Texture2D tex)
+    void ConvertTextureToPng(Texture2D tex, string fileName)
     {
         var pngData = tex.EncodeToPNG();
-        File.WriteAllBytes(filePath, pngData);
+        File.WriteAllBytes(filePath + fileName + ".png", pngData);
     }
     
     private static Vector2 GetMousePositionInLocalSpace(GameObject targetObject)
@@ -170,13 +185,63 @@ public class PixelPaint : MonoBehaviour
             mousePosition.y - position.y);
     }
 
-    private void SetAllTextures()
+    private void Mirror(int index)
+    {
+        MirrorsState[index]++;
+        if (MirrorsState[index] > 5) MirrorsState[index] = 0;
+        MirrorsImage[index].color = MirrorsState[index] switch
+        {
+            0 => Color.white,
+            1 => Color.blue,
+            2 => Color.red,
+            3 => Color.green,
+            4 => Color.yellow,
+            5 => Color.cyan,
+            _ => MirrorsImage[index].color
+        };
+        images[index].sprite = spriteList[MirrorsState[index]];
+    }
+
+    private void SwitchTexture(byte index)
+    {
+        textureNumber = MirrorsState[index];
+        mainImage.sprite = spriteList[MirrorsState[index]];
+    }
+
+    private void initAllImages(byte size)
     {
         foreach (var image in images)
         {
-            image.sprite = Sprite.Create(texture,
-                new Rect(0, 0, texture.width, texture.height),
-                Vector2.zero);;
+            var _texture = new Texture2D(size, size) {
+                filterMode = FilterMode.Point
+            };
+            AllErace(_texture);
+            textureList.Add(_texture);
+            var _sprite = Sprite.Create(_texture,
+                new Rect(0, 0, _texture.width, _texture.height),
+                Vector2.zero);
+            spriteList.Add(_sprite);
+            image.sprite = _sprite;
         }
+    }
+    
+    void Clear()
+    {
+        if (textureList.Count > 0) {
+            textureList.Clear();
+            foreach (var _texture in textureList)
+                Destroy(_texture);
+        }
+
+        if (spriteList.Count > 0) {
+            spriteList.Clear();
+            foreach (var _sprite in spriteList)
+                Destroy(_sprite);
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        Clear();
     }
 }
--- Assets/Symbol/SymbolData.cs
+++
@@ -1,7 +0,0 @@
-public class SymbolData
-{
-    // 以下のデータは他のシーンなどでも流用できる
-    public static string address { get; set; }
-    public static readonly string xymId = "72C0212E67A08BCE";
-    public static float amount { get; set; }
-}
